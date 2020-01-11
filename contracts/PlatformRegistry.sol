31bcd33303de7517b100c79aa21c58817a9d40e3
masaun <masanoriuno@gmail.com>
2020-01-11 11:12:54 +0900

    Add createSreamingMoney() inherited createStream() in Sablier.sol

--- contracts/PlatformRegistry.sol
+++ contracts/PlatformRegistry.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.11;
 pragma experimental ABIEncoderV2;
 
+// Sablier protocol
+import "./sablier-protocol/protocol/contracts/Sablier.sol";
+
 // Storage
 import "./storage/SmStorage.sol";
 import "./storage/SmConstants.sol";
@@ -10,10 +13,24 @@ import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol
 
 contract PlatformRegistry is Ownable, SmStorage, SmConstants {
 
-    constructor() public {}
+    Sablier public sablier;
+
+    constructor(address _sablierContractAddress) public {
+        sablier = Sablier(_sablierContractAddress);
+    }
 
     function testFunc() public returns (bool) {
         return SmConstants.CONFIRMED;
+    }
+
+    function createSreamingMoney(
+        address recipient, 
+        uint256 deposit, 
+        address tokenAddress, 
+        uint256 startTime, 
+        uint256 stopTime
+    ) public returns (bool) {
+        sablier.createStream(recipient, deposit, tokenAddress, startTime, stopTime);
     }
 
 }
--- migrations/4_deploy_platform_registry.js
+++ migrations/4_deploy_platform_registry.js
@@ -1,7 +1,11 @@
 const PlatformRegistry = artifacts.require('PlatformRegistry')
 
+const Sablier = artifacts.require("./Sablier.sol");
+const sablierContractAddress = Sablier.address;
+
 module.exports = async (deployer) => {
   await deployer.deploy(
-    PlatformRegistry
+    PlatformRegistry,
+    sablierContractAddress
   );
 }
