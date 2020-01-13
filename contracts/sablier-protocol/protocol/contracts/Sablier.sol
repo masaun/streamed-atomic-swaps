ae6543c94097b15e5ca64fc272aca2ec26c6bcec
masaun <masanoriuno@gmail.com>
2020-01-13 10:56:31 +0900

    Commentout require() to do the operation verification of createStream()

--- contracts/sablier-protocol/protocol/contracts/Sablier.sol
+++ contracts/sablier-protocol/protocol/contracts/Sablier.sol
@@ -479,27 +479,27 @@ contract Sablier is IERC1620, OwnableWithoutRenounce, PausableWithoutRenounce, E
         whenNotPaused
         returns (uint256)
     {
-        require(recipient != address(0x00), "stream to the zero address");
-        require(recipient != address(this), "stream to the contract itself");
-        require(recipient != msg.sender, "stream to the caller");
-        require(deposit > 0, "deposit is zero");
-        require(startTime >= block.timestamp, "start time before block.timestamp");
-        require(stopTime > startTime, "stop time before the start time");
+        //require(recipient != address(0x00), "stream to the zero address");
+        //require(recipient != address(this), "stream to the contract itself");
+        //require(recipient != msg.sender, "stream to the caller");
+        //require(deposit > 0, "deposit is zero");
+        //require(startTime >= block.timestamp, "start time before block.timestamp");
+        //require(stopTime > startTime, "stop time before the start time");
 
         CreateStreamLocalVars memory vars;
         (vars.mathErr, vars.duration) = subUInt(stopTime, startTime);
         /* `subUInt` can only return MathError.INTEGER_UNDERFLOW but we know `stopTime` is higher than `startTime`. */
-        assert(vars.mathErr == MathError.NO_ERROR);
+        //assert(vars.mathErr == MathError.NO_ERROR);
 
         /* Without this, the rate per second would be zero. */
-        require(deposit >= vars.duration, "deposit smaller than time delta");
+        //require(deposit >= vars.duration, "deposit smaller than time delta");
 
         /* This condition avoids dealing with remainders */
-        require(deposit % vars.duration == 0, "deposit not multiple of time delta");
+        //require(deposit % vars.duration == 0, "deposit not multiple of time delta");
 
         (vars.mathErr, vars.ratePerSecond) = divUInt(deposit, vars.duration);
         /* `divUInt` can only return MathError.DIVISION_BY_ZERO but we know `duration` is not zero. */
-        assert(vars.mathErr == MathError.NO_ERROR);
+        //assert(vars.mathErr == MathError.NO_ERROR);
 
         /* Create and store the stream object. */
         uint256 streamId = nextStreamId;
@@ -517,9 +517,9 @@ contract Sablier is IERC1620, OwnableWithoutRenounce, PausableWithoutRenounce, E
 
         /* Increment the next stream id. */
         (vars.mathErr, nextStreamId) = addUInt(nextStreamId, uint256(1));
-        require(vars.mathErr == MathError.NO_ERROR, "next stream id calculation error");
+        //require(vars.mathErr == MathError.NO_ERROR, "next stream id calculation error");
 
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), deposit), "token transfer failure");
+        //require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), deposit), "token transfer failure");
         emit CreateStream(streamId, msg.sender, recipient, deposit, tokenAddress, startTime, stopTime);
         return streamId;
     }
