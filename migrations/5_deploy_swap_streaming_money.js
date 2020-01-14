5a82213989a53fb162bd09b2fb6db62f7741f00c
masaun <masanoriuno@gmail.com>
2020-01-15 08:47:46 +0900

    Add migration file of SwapStreamingMoney.sol

---
+++ migrations/5_deploy_swap_streaming_money.js
@@ -0,0 +1,12 @@
+const SwapStreamingMoney = artifacts.require('SwapStreamingMoney')
+
+const Sablier = artifacts.require("./Sablier.sol");
+//const sablierContractAddress = "0xc04Ad234E01327b24a831e3718DBFcbE245904CC";  // This is the contract address of Sablier.sol for all of testnet (Ropsten, Rinkeby, Kovan)
+const sablierContractAddress = Sablier.address;
+
+module.exports = async (deployer) => {
+  await deployer.deploy(
+    SwapStreamingMoney,
+    sablierContractAddress
+  );
+}
